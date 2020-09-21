15269f53d8bb35e41c80d304d740e4e871b9a003
Crystal T <ctalley.cat@gmail.com>
2020-09-21 03:18:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,8 +4,8 @@ second_number = 1
 
 sum = (first_number + second_number) 
 
-difference =(first_number - second_number)
+difference = (first_number - second_number)
 
 product = (first_number*second_number)
 
-qoutent - (first_number/second_number)
+qoutent = (first_number/second_number)
